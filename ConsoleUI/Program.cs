5ea01534ded74a48a1da60cd01d336442bc1a8e8
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-01-31 16:32:21 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Business.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            foreach(var product in productManager.GetAll())
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+
+        }
+        
+    }
+
+
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,72 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {               //Convention naming
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            //Like those are comes from Oracle, Sql, DataBase
+        _products = new List<Product> {
+        new Product {CategoryID = 1, ProductId = 1, ProductName = "Car", UnitPrice = 15, UnitsInStock = 65},
+        new Product {CategoryID = 1, ProductId = 2, ProductName = "Glass", UnitPrice = 534, UnitsInStock = 45},
+        new Product {CategoryID = 2, ProductId = 3, ProductName = "Mouse", UnitPrice = 234, UnitsInStock = 678},
+        new Product {CategoryID = 2, ProductId = 4, ProductName = "Hat", UnitPrice = 5, UnitsInStock = 9},
+        new Product {CategoryID = 2, ProductId = 5, ProductName = "Jar", UnitPrice = 16, UnitsInStock = 7},
+
+        };
+        
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+           Product deleteProduct = null;
+            /*   foreach (Product p in _products)
+               {
+                   if(p.CategoryID == product.CategoryID) 
+                   { 
+                       deleteProduct = p; break; 
+                   }
+               } */
+            deleteProduct = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+
+            _products.Remove(deleteProduct);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryID == categoryId).ToList();
+
+        }
+
+        public void Update(Product product)
+        {
+            Product ProductToUpdate;
+            ProductToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            ProductToUpdate.ProductName = product.ProductName;
+            ProductToUpdate.UnitPrice = product.UnitPrice;
+            ProductToUpdate.CategoryID = product.CategoryID; 
+            ProductToUpdate.UnitsInStock = product.UnitsInStock;
+
+        }
+
+    }
+
+}
