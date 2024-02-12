debb56a949bbdb9fdb6ff0141c80b57db3903701
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-02-12 19:43:09 +0300

    Added new project WebAPI Day 11

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosely Coupled
+        //Naming Convention
+        //IoC Container --> Inversion of Control
+        IProductService _ProductService;
+        public ProductsController(IProductService productService)
+        {
+                _ProductService = productService;
+        }
+        [HttpGet]
+        public List<Product> Get()
+        {
+            var result = _ProductService.GetAll();
+            return result.Data;
+
+
+        }
+
+    }
+
+}
---
+++ WebAPI/Program.cs
@@ -0,0 +1,33 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IProductService, ProductManager>(); //ProductService'nin ProductManager olduðunu verdik
+builder.Services.AddSingleton<IProductDal, EfProductDal>();  //ProductDal istenirse ona EfProduct dal ver dedik
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
