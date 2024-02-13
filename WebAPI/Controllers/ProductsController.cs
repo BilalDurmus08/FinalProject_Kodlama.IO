588d9ca72458a833a6c141299e676c929949e991
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-02-13 15:34:31 +0300

    Day 11 final commit

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -19,14 +19,42 @@ namespace WebAPI.Controllers
         {
                 _ProductService = productService;
         }
-        [HttpGet]
-        public List<Product> Get()
+        [HttpGet("getall")]
+        public IActionResult GetAll()
         {
             var result = _ProductService.GetAll();
-            return result.Data;
-
+            if(result.Success == true)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
 
         }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id) 
+        {
+            var result = _ProductService.GetByProductId(id);
+            if(result.Success == true)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _ProductService.Add(product);
+            if(result.Success == true)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+
+        }
+
 
     }
 
